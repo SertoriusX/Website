f630c51baca4b95c0e467c8e3e18de110da76d87
SertoriusX <152284852+SertoriusX@users.noreply.github.com>
2023-11-28 17:29:48 +0100

    Create views.py

---
+++ myApp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render,get_list_or_404
+from .models import yugioh
+from django.core.paginator import Paginator
+
+# Create your views here.
+
+def cards(request,*args,**kwargs):
+    cards = yugioh.objects.all()
+    paginator = Paginator(cards, 16)  # Show 25 contacts per page.
+
+    page_number = request.GET.get("page")
+    cards = paginator.get_page(page_number) 
+    return render(request,'myApp/card.html',{'cards':cards}) 
+
+def detail(request,card_id,*args,**kwargs):
+    cards = get_list_or_404(yugioh,id=card_id)
+    
+  
+    return render(request,'myApp/detail.html',{'cards':cards}) 
+
+
+def home(response):
+    return render(response,'myApp/home.html')
