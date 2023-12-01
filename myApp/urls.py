47e0ae269ced270691751682cdfbc2cc6ae082a5
SertoriusX <152284852+SertoriusX@users.noreply.github.com>
2023-12-01 03:12:46 +0200

    reawork

--- myApp/urls.py
+++ myApp/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name="Index"),
-    path('card/',views.cards,name="Card"),
-    path('detail/<int:cards_id>/',views.detail,name="detail"),
-
+    path('',views.home,name="index"),
+    path('card/',views.cards,name="card"),
+    path('detail/<int:card_id>/',views.detail,name="detail"),
     path('home/',views.home,name="Home"),
 
 
--- myApp/views.py
+++ myApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,get_list_or_404
-from .models import yugioh
+from .models import yugioh,yugioh1
 from django.core.paginator import Paginator
 
 # Create your views here.
@@ -7,14 +7,13 @@ from django.core.paginator import Paginator
 def cards(request,*args,**kwargs):
     cards = yugioh.objects.all()
     paginator = Paginator(cards, 16)  # Show 25 contacts per page.
-
     page_number = request.GET.get("page")
     cards = paginator.get_page(page_number) 
     return render(request,'myApp/card.html',{'cards':cards}) 
 
-def detail(request,cards_id,*args,**kwargs):
-    cards = get_list_or_404(yugioh,id=cards_id)
-    return render(request,'myApp/detail.html',{'cards':cards}) 
+def detail(request,card_id,*args,**kwargs):
+    card = get_list_or_404(yugioh,id=card_id)
+    return render(request,'myApp/detail.html',{'cards':card}) 
 
 
 def home(response):
